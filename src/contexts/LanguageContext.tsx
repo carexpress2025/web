ec79da3c9b26d415dd787c49883ee04ccfcf370d
Car Express <197638789+carexpress2025@users.noreply.github.com>
2025-02-12 11:46:43 -0300

    melhorando o children

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -4,11 +4,11 @@ import { persistor, storeLanguage } from '@/store/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-interface IProps {
+export default function LanguageContext({
+  children,
+}: Readonly<{
   children: React.ReactNode;
-}
-
-export default function LanguageContext({ children }: IProps) {
+}>) {
   return (
     <Provider store={storeLanguage}>
       <PersistGate loading={null} persistor={persistor}>
