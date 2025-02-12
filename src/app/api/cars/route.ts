c0bd24fd6fb14a8ad0b13cfee20cc9262f75fb57
Car Express <197638789+carexpress2025@users.noreply.github.com>
2025-02-12 20:33:25 -0300

    rota api para adicionar um carro

--- src/app/api/cars/route.ts
+++ src/app/api/cars/route.ts
@@ -1,6 +1,77 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 import { Cars } from '@prisma/client';
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  try {
+    const data = await req.json();
+
+    if (!data.idCar || !data.name || !data.brand || !data.year || !data.price) {
+      return NextResponse.json(
+        {
+          error: 'Os campos idCar, name, brand, year e price são obrigatórios',
+        },
+        { status: 400 },
+      );
+    }
+
+    const existingCar = await prisma.cars.findUnique({
+      where: { idCar: data.idCar },
+    });
+
+    if (existingCar) {
+      return NextResponse.json(
+        { error: 'ID do carro já cadastrado' },
+        { status: 400 },
+      );
+    }
+
+    const newCar = await prisma.cars.create({
+      data: {
+        idCar: data.idCar,
+        brand: data.brand,
+        name: data.name,
+        bodyType: data.bodyType,
+        painting: data.painting,
+        carType: data.carType,
+        driveType: data.driveType,
+        transmissionType: data.transmissionType,
+        color: data.color,
+        mileage: data.mileage,
+        engine: data.engine,
+        engineDisplacement: data.engineDisplacement,
+        fuelType: data.fuelType,
+        safety: data.safety,
+        interior: data.interior,
+        electronics: data.electronics,
+        owners: data.owners,
+        status: data.status,
+        year: data.year,
+        inspected: data.inspected,
+        plate: data.plate,
+        additionalInformation: data.additionalInformation,
+        others: data.others,
+        roadWorthy: data.roadWorthy,
+        price: data.price,
+        images: data.images || [],
+        phone: data.phone,
+        seller: data.seller,
+        link: data.link,
+      },
+    });
+
+    return NextResponse.json(
+      { message: 'Carro cadastrado com sucesso', data: newCar },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error('Erro ao criar carro:', error);
+    return NextResponse.json(
+      { error: 'Erro interno do servidor' },
+      { status: 500 },
+    );
+  }
+}
 
 export async function GET(): Promise<NextResponse> {
   try {
