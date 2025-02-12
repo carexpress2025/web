e34428a9baa7fda6a17012e4cf20da71599fcb9a
Car Express <197638789+carexpress2025@users.noreply.github.com>
2025-02-12 15:55:01 -0300

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,18 @@
-import { auth } from './auth';
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
-export default auth((req) => {
-  // Callback function
-});
+const secret = process.env.NEXTAUTH_SECRET;
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret });
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/signin', req.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/dashboard', '/cars'],
 };
