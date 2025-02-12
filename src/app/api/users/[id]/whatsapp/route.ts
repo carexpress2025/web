41ea1d3b54921ee9f03a1e24bd2ffdfe92bfe19b
Car Express <197638789+carexpress2025@users.noreply.github.com>
2025-02-12 17:45:17 -0300

    criando configuração do whatsapp

---
+++ src/app/api/users/[id]/whatsapp/route.ts
@@ -0,0 +1,61 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { id: string } },
+): Promise<NextResponse> {
+  try {
+    const { id: userId } = params;
+
+    const { number, session } = await req.json();
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: 'User ID é obrigatório' },
+        { status: 400 },
+      );
+    }
+
+    if (!number || !session) {
+      return NextResponse.json(
+        { error: 'O número e a sessão são obrigatórios' },
+        { status: 400 },
+      );
+    }
+
+    const existingWhatsapp = await prisma.whatsapps.findUnique({
+      where: { number: number },
+    });
+
+    if (existingWhatsapp) {
+      return NextResponse.json(
+        { error: 'Número de WhatsApp já cadastrado' },
+        { status: 400 },
+      );
+    }
+
+    const newWhatsapp = await prisma.whatsapps.create({
+      data: {
+        number,
+        session,
+        UsersWhatsapp: {
+          create: {
+            userId: userId,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(
+      { message: 'WhatsApp cadastrado com sucesso', data: newWhatsapp },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error('Erro ao criar WhatsApp:', error);
+    return NextResponse.json(
+      { error: 'Erro interno do servidor' },
+      { status: 500 },
+    );
+  }
+}
