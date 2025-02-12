95e158678ba93d68672446baf6977f8d9270a41e
Car Express <197638789+carexpress2025@users.noreply.github.com>
2025-02-12 16:06:56 -0300

    removendo rota que traz todos os usuario

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -32,18 +32,3 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
     );
   }
 }
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function GET(req: NextRequest): Promise<NextResponse> {
-  try {
-    const users = await prisma.users.findMany();
-
-    return NextResponse.json(users, { status: 200 });
-  } catch (error: unknown) {
-    console.error('Erro ao buscar usuários:', error);
-    return NextResponse.json(
-      { error: 'Erro interno do servidor' },
-      { status: 500 },
-    );
-  }
-}
