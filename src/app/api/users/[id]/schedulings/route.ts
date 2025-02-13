78c071ba5e80472e27620d168afa7d77529c48c1
Car Express <197638789+carexpress2025@users.noreply.github.com>
2025-02-13 10:10:52 -0300

    rota para atualizar o agendamento

--- src/app/api/users/[id]/schedulings/route.ts
+++ src/app/api/users/[id]/schedulings/route.ts
@@ -44,3 +44,66 @@ export async function GET(
     );
   }
 }
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { userId: string; schedulingId: string } },
+): Promise<NextResponse> {
+  try {
+    const { userId, schedulingId } = params;
+    const updates = await req.json();
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: 'User ID é obrigatório' },
+        { status: 400 },
+      );
+    }
+
+    if (!schedulingId) {
+      return NextResponse.json(
+        { error: 'Scheduling ID é obrigatório' },
+        { status: 400 },
+      );
+    }
+
+    const schedulingExists = await prisma.userSchedulings.findUnique({
+      where: {
+        schedulingId,
+        userId,
+      },
+      include: {
+        scheduling: true,
+      },
+    });
+
+    if (!schedulingExists) {
+      return NextResponse.json(
+        { error: 'Agendamento não encontrado para este usuário' },
+        { status: 404 },
+      );
+    }
+
+    const updatedScheduling = await prisma.schedulings.update({
+      where: { id: schedulingId },
+      data: {
+        ...updates,
+        updatedAt: new Date(),
+      },
+    });
+
+    return NextResponse.json(
+      {
+        message: 'Agendamento atualizado com sucesso',
+        data: updatedScheduling,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error('Erro ao atualizar agendamento:', error);
+    return NextResponse.json(
+      { error: 'Erro interno do servidor' },
+      { status: 500 },
+    );
+  }
+}
