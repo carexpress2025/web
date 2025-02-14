13664b1089006896b03b8a649f7f236a86f78157
Car Express <197638789+carexpress2025@users.noreply.github.com>
2025-02-14 12:49:19 -0300

    interfaces do prisma

--- src/interfaces/prisma/ICarInterface.d.ts
+++ src/interfaces/prisma/ICarInterface.d.ts
@@ -1,14 +1,14 @@
-import type { CAR_BODY_TYPE } from '../enums/CarBodyType';
-import type { CAR_BRAND } from '../enums/CarBrand';
-import type { CAR_COLOR } from '../enums/CarColor';
-import type { CAR_FUELTYPE } from '../enums/CarFuelType';
-import type { CAR_PAINTING_TYPE } from '../enums/CarPaintingType';
-import type { CAR_TRANSMISSION_TYPE } from '../enums/CarTransmissionType';
-import type { IBaseInterface } from './base/IBaseInterface';
-import type { ICarElectronicsInterface } from './ICarElectronicsInterface';
-import type { ICarInteriorInterface } from './ICarInteriorInterface';
-import type { ICarSafetyInterface } from './ICarSafetyInterface';
-import type { ICarSpecificationsInterface } from './ICarSpecificationsInterface';
+import type { CAR_BODY_TYPE } from '../../enums/CarBodyType';
+import type { CAR_BRAND } from '../../enums/CarBrand';
+import type { CAR_COLOR } from '../../enums/CarColor';
+import type { CAR_FUELTYPE } from '../../enums/CarFuelType';
+import type { CAR_PAINTING_TYPE } from '../../enums/CarPaintingType';
+import type { CAR_TRANSMISSION_TYPE } from '../../enums/CarTransmissionType';
+import type { IBaseInterface } from '../base/IBaseInterface';
+import type { ICarElectronicsInterface } from '../ICarElectronicsInterface';
+import type { ICarInteriorInterface } from '../ICarInteriorInterface';
+import type { ICarSafetyInterface } from '../ICarSafetyInterface';
+import type { ICarSpecificationsInterface } from '../ICarSpecificationsInterface';
 
 export interface ICarInterface extends IBaseInterface {
   brand: CAR_BRAND;
