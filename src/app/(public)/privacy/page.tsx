5382e9f996acd94d80200d9fcc0b66c6be7e6671
Car Express <197638789+carexpress2025@users.noreply.github.com>
2025-02-14 20:00:52 -0300

    Páginas publicas

--- src/app/(public)/privacy/page.tsx
+++ src/app/(public)/privacy/page.tsx
@@ -1,9 +1,10 @@
 import { JSX } from 'react';
+import SectionsView from '../components/SectionsView';
 
 export default function Page(): JSX.Element {
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <main className="ml-64 flex-1 bg-gray-100 p-8">
+      <SectionsView mainKey="privacy" />
+    </main>
   );
 }
