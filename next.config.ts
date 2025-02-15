f8966aa39ffe9af97febe8f6f7fd7a139514444e
Car Express <197638789+carexpress2025@users.noreply.github.com>
2025-02-15 20:46:38 -0300

    revert(redirect): removendo o redirect do / para /dashboard

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/', // Quando a rota for "/"
-        destination: '/dashboard', // Redireciona para "/home"
-        permanent: true, // Define como permanente (status 308)
-      },
-      {
-        source: '/home', // Quando a rota for "/dashboard"
-        destination: '/dashboard', // Redireciona para "/home"
-        permanent: true, // Define como permanente (status 308)
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
