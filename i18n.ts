a5c4a6fe47a0b4aeb90c394a015982908deaf2c8
Car Express <197638789+carexpress2025@users.noreply.github.com>
2025-02-15 20:53:50 -0300

    chore(locales): movendo os locales

--- i18n.ts
+++ i18n.ts
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import en from './src/locales/en_US.json';
-import fi from './src/locales/fi_FI.json';
-import br from './src/locales/pt_BR.json';
+import en from './src/presentation/locales/en_US.json';
+import fi from './src/presentation/locales/fi_FI.json';
+import br from './src/presentation/locales/pt_BR.json';
 
 i18n.use(initReactI18next).init({
   resources: {
