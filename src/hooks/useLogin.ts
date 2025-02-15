df2d6c6fdd46bc1d258b25bb9906071690fd4631
Car Express <197638789+carexpress2025@users.noreply.github.com>
2025-02-15 13:12:50 -0300

    chore(pagina de login): melhorando a pagina de login de usuario

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import { signIn } from 'next-auth/react';
+import { useTranslation } from 'react-i18next';
+import { useSearchParams } from 'next/navigation';
+import { signinAccountSchema } from '@/validations/signinAccountSchema';
+
+export function useLogin() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const { t } = useTranslation();
+  const searchParams = useSearchParams();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const validationResult = signinAccountSchema.safeParse({ email, password });
+
+    if (!validationResult.success) {
+      const validationError = validationResult.error.format();
+      const firstError = Object.values(validationError)
+        .flat()
+        .map((err) => (typeof err === 'string' ? err : err._errors[0]))[0];
+
+      setError(firstError);
+      return;
+    }
+
+    setLoading(true);
+
+    const callbackUrl = searchParams.get('callbackUrl') || '/';
+
+    try {
+      const response = await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl,
+      });
+
+      setLoading(false);
+
+      if (response?.error) {
+        setError(t('messages.errors.auth.signin.failedLogin'));
+      } else if (!response?.ok) {
+        setError(t('messages.errors.auth.signin.failedLogin'));
+      }
+    } catch (error) {
+      console.error('Erro ao tentar autenticar:', error);
+      setError(t('messages.errors.auth.signin.generalError'));
+      setLoading(false);
+    }
+  };
+
+  return {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    error,
+    loading,
+    handleSubmit,
+  };
+}
