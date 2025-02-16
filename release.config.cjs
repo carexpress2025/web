acef9f384a0b95e6a979fbdb04dccb7b159a9055
Car Express <197638789+carexpress2025@users.noreply.github.com>
2025-02-15 21:11:28 -0300

    chore(config do release): configurando o release.js

---
+++ release.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    '@semantic-release/git',
+    '@semantic-release/github',
+  ],
+};
